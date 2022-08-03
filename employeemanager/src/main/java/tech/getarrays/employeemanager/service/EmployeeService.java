bcc8c4713849be838ef8a6f0f0db3f39af0c0722
Atul-saini <atul47145@gmail.com>
2022-08-03 09:41:52 +0530

    backend commit

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package tech.getarrays.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repo.EmployeeRepo;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public  Employee addEmployee(Employee employee)
+    {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee)
+    {
+        return employeeRepo.save(employee);
+    }
+
+    public Object findEmployeeById(Long id) throws Throwable {
+
+        return  employeeRepo.findEmployeeById(id);
+    }
+
+    public void deleteEmployee(Long id)
+    {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
